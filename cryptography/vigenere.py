fe807e788f5484e0d5f7813d453e4d56936e34f9
warren fung <warrenfung10@gmail.com>
2018-05-22 20:56:01 -0700

    math broken for vigenere

--- cryptography/vigenere.py
+++ cryptography/vigenere.py
@@ -5,7 +5,35 @@ import sys
 import string
 
 def vigenere(arg1, arg2):
-    print("a1 %s a2" % arg1, arg2)
+    key_list = list(arg1)
+    print(key_list)
+    key_len = len(arg1)
+    i = 0
+    result = []
+    for letter in arg2:
+        print("letter %c" % letter)
+        if letter.isalpha():
+            upper = 0
+            if letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+                upper = 1
+            print("letter %c %i keylist %c %i" % (letter, ord(letter), key_list[i], ord(key_list[i])))
+            #pew = ord(letter) - 97 + ord(key_list[i]) - 97
+            index = ord(letter) - 97
+            move = ord(key_list[i]) - 97
+            print("move = %i" % move)
+            print("index = %i" % index)
+            pew = index + move
+            if pew > 26:
+                pew %= 26
+            result.append(ord(letter) + pew)
+            i += 1
+        else:
+            print("else %c" % letter)
+            result.append(letter)
+        print("%s i = %i" % (result, i))
+        if i > key_len:
+            i = 0
+    print(result)
 
 def main():
     arg_len = len(sys.argv) - 1
